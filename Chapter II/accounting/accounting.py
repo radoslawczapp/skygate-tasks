df09cf5d988797d8376d7e20ded1cda08d0bb2d6
Radek <czappradoslaw@gmail.com>
2020-01-30 15:50:48 +0100

    Task no.2: Accounting testing

--- Chapter II/accounting/accounting.py
+++ Chapter II/accounting/accounting.py
@@ -1,18 +1,22 @@
 import re
 
-# Define a path to the file
-file_name = "skychallenge_accounting_input.txt"
 
-# Open this file using context manager
-with open(file_name, 'r') as f:
-    # Save data from the file into the variable
-    data = f.read()
+def get_sum(json_string):
+    # Create a list of numbers by using RegEx
+    numbers_list = re.findall(r'[+-]?\d+(?:\.\d+)?', str(json_string))
 
-# Create a list of numbers by using RegEx
-numbers_list = re.findall(r'[+-]?\d+(?:\.\d+)?', data)
+    # Create a list of all numbers from the file by using list comprehension
+    numbers = [int(x) for x in numbers_list]
+    return sum(numbers)
 
-# Create a list of all numbers from the file by using list comprehension
-numbers = [int(x) for x in numbers_list]
 
-# Return the sum of all numbers from the file
-print("Sum of all numbers: ", sum(numbers))
+if __name__ == '__main__':
+    # Define a path to the file
+    file_name = "skychallenge_accounting_input.txt"
+    con = '[-10, 0, 100]'
+    # Open this file using context manager
+    with open(file_name, 'r') as f:
+        # Save data from the file into the variable
+        f_data = f.read()
+    # Return the sum of all numbers from the file
+    print("Sum of all numbers: ", get_sum(con))
--- Chapter II/accounting/test_accounting.py
+++ Chapter II/accounting/test_accounting.py
@@ -1,2 +1,45 @@
 import unittest
-from .accounting import
+from accounting import get_sum
+
+
+class TestAccounting(unittest.TestCase):
+    def test_none(self):
+        self.assertEqual(get_sum(None), 0)
+
+    def test_bool(self):
+        self.assertEqual(get_sum(True), 0)
+        self.assertEqual(get_sum(False), 0)
+
+    def test_empty(self):
+        self.assertEqual(get_sum(''), 0)
+        self.assertEqual(get_sum([]), 0)
+        self.assertEqual(get_sum({}), 0)
+
+    def test_number(self):
+        self.assertEqual(get_sum(15), 15)
+        self.assertEqual(get_sum(-2), -2)
+        self.assertEqual(get_sum(0), 0)
+
+    def test_string(self):
+        self.assertEqual(get_sum('string'), 0)
+        self.assertEqual(get_sum('string}[]{'), 0)
+        self.assertEqual(get_sum('string+-+'), 0)
+
+    def test_list(self):
+        self.assertEqual(get_sum(['a', -10, 'b', 'c', 100]), 90)
+        self.assertEqual(get_sum([-10, 0, 100]), 90)
+        self.assertEqual(get_sum([-10, True, 100]), 90)
+
+    def test_dict(self):
+        self.assertEqual(get_sum({'x': -10, 'y': 0, 'z': 100}), 90)
+        self.assertEqual(get_sum({'x': -10, 'y': 100, 'z': 'string'}), 90)
+        self.assertEqual(get_sum({'a': 100, 'x': -10, 'y': False, 'z': 'string'}), 90)
+
+    def test_nesting(self):
+        self.assertEqual(get_sum([-10, [0], [[100]]]), 90)
+        self.assertEqual(get_sum({'a': {'b': -10}, 0: 100}), 90)
+        self.assertEqual(get_sum({'a': 'string+-10', 'b': [0, 100]}), 90)
+
+
+if __name__ == '__main__':
+    unittest.main()
